e573d070544deec6beaddf1425c495f04e908b53
dul-cet <a_moldir@kbtu.kz>
2025-04-15 12:34:39 +0500

    new

--- blog-platform/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PostListComponent } from './post-list/post-list.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'posts', component: PostListComponent }
-];
